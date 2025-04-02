0a5234e65d0e2bed1c6227cfe453b410492812db
Steven Schwab <schwabs@umich.edu>
2025-04-02 15:50:11 -0400

    write createUser test

--- api/users/users-model.spec.js
+++ api/users/users-model.spec.js
@@ -33,4 +33,35 @@ describe('users model', () => {
             expect(users).toHaveLength(2);
         });
     })
+
+    describe('getUserById(id)', () => {
+        test.todo('can find a user by id')
+    })
+
+    describe('createUser(username, email)', () => {
+        it('inserts provided user into db', async () => {
+            await User.createUser('schwabs', 'steven.schwab1@gmail.com')
+            let users = await User.getAllUsers()
+            expect(users).toHaveLength(1)
+
+            await User.createUser('hcraven', 'hcraven7@gmail.com')
+            users = await User.getAllUsers()
+            expect(users).toHaveLength(2)
+        })
+
+        it('gives back the inserted user', async () => {
+            let user = await User.createUser('schwabs', 'steven.schwab1@gmail.com')
+            expect(user).toMatchObject({ id: 1, username: 'schwabs', email: 'steven.schwab1@gmail.com' })
+            user = await User.createUser('gaffer', 'sam@gmail.com')
+            expect(user).toMatchObject({ id: 2, username: 'gaffer', email: 'sam@gmail.com'})
+        })
+    })
+
+    describe('updateUser(id, username, email)', () => {
+        test.todo('updates the user')
+    })
+
+    describe('deleteUserById(id)', () => {
+        test.todo('deletes the user')
+    })
 })
