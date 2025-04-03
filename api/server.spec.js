799bdea00a836ff3c35bae10dc6edc587eed25c4
Steven Schwab <schwabs@umich.edu>
2025-04-03 11:23:01 -0400

    describe the get method general route in tests

--- api/server.spec.js
+++ api/server.spec.js
@@ -19,4 +19,41 @@ describe('server.js', () => {
     it('should set testing environment', () => {
         expect(process.env.NODE_ENV).toBe('testing')
     })
+
+    describe('[GET] /', () => {
+        it('should return 200 OK', () => {
+            // explain that we need to return the call to request()
+            // this signals to jest that this test is asynchronous and it needs
+            // to wait for the promise to resolve, before running the assertions
+            return request(server)
+                .get('/')
+                .then(res => {
+                    // the response object has useful methods we can use
+                    expect(res.status).toBe(200)
+                })
+        })
+        // using the squad (async/await) we don't need to return anything
+        // jes will wait because of the async function
+        it('should return 200 OK using async await', async () => {
+            const res = await request(server).get('/')
+            expect(res.status).toBe(200)
+        })
+        it('should return JSON', async () => {
+            const res = await request(server).get('/')
+            expect(res.type).toBe('application/json')
+        })
+        it('should return { api: "up" }', async () => {
+            const res = await request(server).get('/')
+            expect(res.body).toEqual({ api: 'up' })
+        })
+        it('[GET] / another way of doing it', () => {
+            return request(server)
+                .get('/')
+                .expect('Content-Type', /json/)
+                .expect('Content-Length', '12')
+                .expect(200)
+        })
+    })
+
+
 })
